145809feb07bb098524911e337ec6eaa43b502d6
Madshtik <jann_cosio@yahoo.com>
2020-03-28 22:58:26 +0400

    Added Server to Client connection
    -Client (Player) can now send a message to the Server

--- server.js/server.js/app.js
+++ server.js/server.js/app.js
@@ -1,10 +1,23 @@
 'use strict';
 
 var port = /*process.env.PORT ||*/ 1337;
+
+/**
+ * @type {SocketIO.Socket[]} the currently connected players
+ * */
+var playerConnections = [];
+
 const socket = require('socket.io')(port);
 
+console.log("Port Number:", port);
 console.log("Server Started");
 
 socket.on("connection", (soc) => {
     console.log("Connection Made With Client")
+
+    playerConnections.push(soc);
+
+    soc.on("I am connecting to you", () => {
+        console.log("You are connected")
+    })
 })
