607bb118eaf26ecb816d3f38e3214ca43e0c86a8
Madshtik <jann_cosio@yahoo.com>
2020-04-04 08:25:52 +0400

    Added 2 player multiplayer

--- server.js/server.js/app.js
+++ server.js/server.js/app.js
@@ -1,24 +1,130 @@
 'use strict';
 
 var port = /*process.env.PORT ||*/ 1337;
-
-/**
- * @type {SocketIO.Socket[]} the currently connected players
- * */
-var playerConnections = [];
+var currentId = 0;
 
 const socket = require("socket.io")(port);
 
 console.log("Port Number:", port);
 console.log("Server Started");
 
-socket.on("connection", (soc) => {
-    console.log("Connection Made With Client");
+/**
+ * @type {Player[]} array to store new players
+ * */
+playersArr = [];
 
-    playerConnections.push(soc);
+class Player
+{
+    id;
+    position;
+    rotation;
 
-    soc.on("Sending My Position", (data) => {
-        console.log(data);
-        playerConnections[0].emit("PlayerMove", data);
+    /**
+     * @type {SocketIO.Socket}  this is the socket given when a player joins
+     */
+    socket;
+
+    /**
+     * @param {number} playerId this is the player ID given when a player joins
+     * @param {SocketIO.Socket} playerSocket this is the socket when a player joins
+     */
+    constructor(playerId, playerSocket)
+    {
+        this.id = playerId;
+        this.socket = playerSocket;
+    }
+}
+
+class Room
+{
+    /**
+     * @type {Player}  this is the player type
+     */
+    overlord;
+
+    /**
+     * @type {Player}  this is the player type
+     */
+    joe;
+
+
+    /**
+     * 
+     * @param {Player[]} players this is an array of players
+     */
+    constructor(players)
+    {
+        //---------- Player Overlord
+        this.overlord = players[0];
+        this.overlord.socket.on("shoot", (data) => //sender
+        {
+            this.joe.socket.emit("shoot", data); //sendee
+        });
+
+        players[0].socket.on("transformUpdate", (data) =>
+        {
+            players[1].socket.emit("transformUpdate", data);
+        });
+
+        players[0].socket.on("giveUp", (data) =>
+        {
+
+        });
+
+        //---------- Player Joe
+        this.joe = players[1];
+        this.joe.socket.on("pickUp", (data) => //sender
+        {
+            this.overlord.socket.emit("pickUp", data); //sendee
+        });
+
+        players[1].socket.on("transformUpdate", (data) =>
+        {
+            players[0].socket.emit("transformUpdate", data);
+        });
+
+        players[1].socket.on("giveUp", (data) =>
+        {
+            
+        });
+
+        //function <= attempt this on everything above to make it look cleaner
+
+        for (var i = 0; i < players.length; i++)
+        {
+            players[i].socket.on("disconnect", (socket) =>
+            {
+                if (this.overlord.socket === socket)
+                {
+                    this.joe.socket.emit("youWin")
+                }
+                else
+                {
+                    this.overlord.socket.emit("youWin")
+                }
+            });
+        }
+        
+    }
+}
+
+socket.on("connection", (soc) =>
+{
+    var newPlayer = new Player(currentId++, soc)
+
+    waitingForRoom = undefined;
+    playersArr.push(newPlayer);
+
+    soc.on("findRoom", () => //matchmaking
+    {
+        if (waitingForRoom === undefined)
+        {
+            waitingForRoom = newPlayer;
+        }
+        else
+        {
+            var match = new Room([newPlayer, waitingForRoom])
+            waitingForRoom = undefined;
+        }
     })
-})
+});
