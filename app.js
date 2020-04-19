e3f32aebd472c638f615393df1cf343676e53d9a
Madshtik <jann_cosio@yahoo.com>
2020-04-19 20:31:52 +0400

    attempting to get data using GetField in console.log for Heroku test

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ class Room
         this.overlord.socket.on("oLTransformUpdate", (data) => //from sender
         {
             this.joe.socket.emit("oLTransformUpdate", data); //to receiver - the Networked OL should receive this
+            console.log(data.GetField("x position"));
         });
 
         this.overlord.socket.on("shoot", (data) =>
@@ -77,7 +78,7 @@ class Room
         this.joe.socket.on("joeTransformUpdate", (data) =>
         {
             this.overlord.socket.emit("joeTransformUpdate", data); //to receiver - the Networked Joe should receive this
-            console.log(data);
+            console.log(data.GetField("x position"));
         });
 
         this.joe.socket.on("pickUp", (data) =>
